411418072d235e362d2c153fdd47a202651dba99
santosh 2 <santehero8@gmail.com>
2023-02-05 17:01:39 +0545

    meta

--- src/components/MetaDecorator/MetaDecorator.jsx
+++ src/components/MetaDecorator/MetaDecorator.jsx
@@ -7,12 +7,13 @@ const MetaDecorator = ({ image, title, description }) => {
                 <title>{title}</title>
                 <meta name="title" content={title} />
                 <meta name="description" content={description} />
-                {/* <meta property="og:type" content="website" /> */}
+                <meta property="og:type" content="website" />
                 <meta property="og:url" content={window.location.href} />
-                <meta property="og:title" content={title} />
-                <meta property="og:description" content={description} />
+                <meta property="og:title" content={"this is the title hahah"} />
+                <meta property="og:description" content={"how are  you i am fine "} />
                 <meta property="og:image" content={image} />
                 <meta property="twitter:card" content="summary_large_image" />
+
             </Helmet>
             {/* 
 
