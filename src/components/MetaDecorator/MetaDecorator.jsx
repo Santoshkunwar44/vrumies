c5547d6c585c96838410c101d37f7285e39c7f01
santosh 2 <santehero8@gmail.com>
2023-02-05 17:12:07 +0545

    meta changes

--- src/components/MetaDecorator/MetaDecorator.jsx
+++ src/components/MetaDecorator/MetaDecorator.jsx
@@ -4,7 +4,13 @@ const MetaDecorator = ({ image, title, description }) => {
     return (
         <>
             <Helmet>
-                <title>{title}</title>
+                <meta property="og:url" content={window.location.href} />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+                <meta property="og:image" content={image} />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+                {/* <title>{title}</title>
                 <meta name="title" content={title} />
                 <meta name="description" content={description} />
                 <meta property="og:type" content="website" />
@@ -13,15 +19,12 @@ const MetaDecorator = ({ image, title, description }) => {
                 <meta property="og:description" content={"how are  you i am fine "} />
                 <meta property="og:image" content={image} />
                 <meta property="twitter:card" content="summary_large_image" />
-
+                <meta property="twitter:url" content={window.location + window.location.search} />
+                <meta property="twitter:title" content={title} />
+                <meta property="twitter:description" content={description
+                } />
+                <meta property="twitter:image" content={image}></meta> */}
             </Helmet>
-            {/* 
-
-            <meta property="twitter:url" content={window.location + window.location.search} />
-            <meta property="twitter:title" content={title} />
-            <meta property="twitter:description" content={description
-            } />
-            <meta property="twitter:image" content={image}></meta> */}
         </>
     )
 }
