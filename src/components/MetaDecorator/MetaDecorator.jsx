dd5a6ce1db0e37fea5bc9e070332cc75f6f36ce3
santosh 2 <santehero8@gmail.com>
2023-02-06 16:36:40 +0545

    hello

--- src/components/MetaDecorator/MetaDecorator.jsx
+++ src/components/MetaDecorator/MetaDecorator.jsx
@@ -11,7 +11,6 @@ const MetaDecorator = ({ image, title, description }) => {
                 <meta name='title' property="og:title" content="buy this flowers" />
                 <meta name='description' property="og:description" content="buy this flowrs " />
                 <meta property="og:image" content="https://scontent.fbwa1-1.fna.fbcdn.net/v/t1.15752-9/324926288_3440684446168532_8492699774424784759_n.png?_nc_cat=111&ccb=1-7&_nc_sid=ae9488&_nc_ohc=I3ofvt1uLLQAX9x2kfd&_nc_ht=scontent.fbwa1-1.fna&oh=03_AdROj7KAHW2PIt6X0gPsHGvUoPaZJP34siUmAqSQE6NutA&oe=64084FAD" />
-
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="https://vrumies2.netlify.app" />
                 <meta property="twitter:title" content="buy this flowers" />
